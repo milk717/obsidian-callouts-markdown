bdae29908b60af6aa11af9ff29baceafe96e90c6
Sumin <robolindasoo@gmail.com>
2024-02-29 16:39:06 +0900

    :heavy_plus_sign: add svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import {mdx} from '@cyco130/vite-plugin-mdx';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mdx()],
+  plugins: [react(), mdx(), svgr()],
 });
