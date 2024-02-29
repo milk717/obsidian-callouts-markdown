73006dcc5533c18a809dcdbe7bf4355299b2c381
Sumin <robolindasoo@gmail.com>
2024-02-29 12:08:19 +0900

    :tada: create mdx-obsidian-callout project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
