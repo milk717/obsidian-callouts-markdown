9f0c37375016536a83743c4d26008755f7757ed2
Sumin <robolindasoo@gmail.com>
2024-03-01 17:37:07 +0900

    :heavy_plus_sign: add vite-plugin-css-injected-by-js

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import {mdx} from '@cyco130/vite-plugin-mdx';
 import svgr from 'vite-plugin-svgr';
 import dts from 'vite-plugin-dts';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,5 +18,11 @@ export default defineConfig({
       external: ['react', 'react-dom'],
     },
   },
-  plugins: [react(), mdx(), svgr(), dts({rollupTypes: true})],
+  plugins: [
+    react(),
+    mdx(),
+    svgr(),
+    dts({rollupTypes: true}),
+    cssInjectedByJsPlugin(),
+  ],
 });
