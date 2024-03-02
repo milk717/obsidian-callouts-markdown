b66376e4be24a337823fc6b87e70714c28face73
Sumin <robolindasoo@gmail.com>
2024-03-03 00:38:17 +0900

    :wrench: Integrate tsconfig common properties into tsconfig.base.json

---
+++ packages/lib/vite.config.ts
@@ -0,0 +1,21 @@
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import {mdx} from '@cyco130/vite-plugin-mdx';
+import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      name: 'mdx-obsidian-callout',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      input: './src/index.ts',
+      external: ['react', 'react-dom'],
+    },
+  },
+  plugins: [react(), mdx(), svgr(), tsconfigPaths()],
+});
