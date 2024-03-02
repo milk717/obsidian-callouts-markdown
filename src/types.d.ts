a0ccf288920f8a96baa580f93a4a93e32b447db1
Sumin <robolindasoo@gmail.com>
2024-03-02 13:04:22 +0900

    :heavy_plus_sign: add mdx-js/runtime

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,16 @@
 declare module '*.mdx' {
-  let MDXComponent: (props: any) => JSX.Element;
+  let MDXComponent: (props: never) => Element;
   export default MDXComponent;
+}
+
+declare module '@mdx-js/runtime' {
+  import {ComponentType} from 'react';
+
+  interface MDXRuntimeProps {
+    children: string;
+    components?: {[elementName: string]: ComponentType<never>};
+  }
+
+  const MDXRuntime: ComponentType<MDXRuntimeProps>;
+  export default MDXRuntime;
 }
