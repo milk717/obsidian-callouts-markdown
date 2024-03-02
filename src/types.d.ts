817b81aaebb9d452bdccabcfe8a835e49756f306
Sumin <robolindasoo@gmail.com>
2024-03-02 13:36:26 +0900

    :heavy_minus_sign: remove mdx-js/runtime

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,15 +2,3 @@ declare module '*.mdx' {
   let MDXComponent: (props: never) => Element;
   export default MDXComponent;
 }
-
-declare module '@mdx-js/runtime' {
-  import {ComponentType} from 'react';
-
-  interface MDXRuntimeProps {
-    children: string;
-    components?: {[elementName: string]: ComponentType<never>};
-  }
-
-  const MDXRuntime: ComponentType<MDXRuntimeProps>;
-  export default MDXRuntime;
-}
