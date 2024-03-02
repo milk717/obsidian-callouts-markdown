25919bc352fd48ff1152923aee874db1f3a53ca1
Sumin <robolindasoo@gmail.com>
2024-03-02 13:46:13 +0900

    :wrench: edit jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.svg\\?react$': '<rootDir>/src/tests/__mocks__/svgMock.tsx',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 };
