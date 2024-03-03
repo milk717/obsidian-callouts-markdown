93cdce8974e7363415070fb0731796e0178cd4d7
Sumin <robolindasoo@gmail.com>
2024-03-03 21:30:18 +0900

    :green_heart: mdx 플러그인 삭제
    Signed-off-by: Sumin <robolindasoo@gmail.com>

--- packages/obsidian-callouts-markdown/vite.config.ts
+++ packages/obsidian-callouts-markdown/vite.config.ts
@@ -1,6 +1,5 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-import {mdx} from '@cyco130/vite-plugin-mdx';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import dts from 'vite-plugin-dts';
@@ -21,7 +20,6 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    mdx(),
     svgr(),
     dts({rollupTypes: true}),
     cssInjectedByJsPlugin(),
