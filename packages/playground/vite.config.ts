62ac58e994cfd3b67ce0eae2dd2914d229b3754b
Sumin <robolindasoo@gmail.com>
2024-03-03 21:17:04 +0900

    :green_heart: Remove mdx from playground site
    Signed-off-by: Sumin <robolindasoo@gmail.com>

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,10 +1,9 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-import {mdx} from '@cyco130/vite-plugin-mdx';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mdx(), svgr(), tsconfigPaths()],
+  plugins: [react(), svgr(), tsconfigPaths()],
 });
